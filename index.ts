af2fb3fb355f613fc460304cfeaf72d65dd0cce6
rajorokit <160819831+rajorokit@users.noreply.github.com>
2024-02-22 15:26:31 +0500

    Add files via upload

---
+++ index.ts
@@ -0,0 +1,43 @@
+let r1 = "saqib raza"
+
+console.log(r1)
+console.log(r1)
+console.log(r1)
+console.log(r1)
+console.log(r1)
+
+let a1 = 123456789
+
+console.log(a1)
+console.log(a1)
+console.log(a1)
+console.log(a1)
+console.log(a1)
+
+let b2 = 45678890
+
+console.log(b2)
+console.log(b2)
+console.log(b2)
+console.log(b2)
+console.log(b2)
+
+
+
+let s6 =67890
+
+console.log(s6)
+console.log(s6)
+console.log(s6)
+console.log(s6)
+console.log(s6)
+console.log(s6)
+
+
+
+
+let A1 =786900
+console.log(a1)
+
+
+
